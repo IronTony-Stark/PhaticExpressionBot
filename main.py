640ceed405df8691aaa01200fa765c5e65832200
BlaD200 <blad600@gmail.com>
2020-09-20 22:59:46 +0300

    Added counting to replies, so they won't repeat several times in a row.

--- main.py
+++ main.py
@@ -1,29 +1,48 @@
 import random
 import re
-from typing import Optional
+from typing import Optional, Dict, List, Tuple
 
 from parse import parse_templates, parse_defaults
 
 templates = parse_templates("templates.txt")
+templates_used: List[Tuple[List[str], Dict[str, int]]] =\
+    [(statements, {answer: 0 for answer in answers}) for statements, answers in templates]
 defaults = parse_defaults("defaults.txt")
-# print(templates)
+defaults_used: Dict[str, int] = {k: 0 for k in defaults}
+
+
+# print(templates_used)
 # print(defaults)
 
 
+def get_min_used_replies(possible_replies: Dict[str, int]) -> List[str]:
+    min_used = min(possible_replies.values())
+    return [ans for ans, used_count in possible_replies.items() if used_count == min_used]
+
+
 def get_default_answer() -> str:
-    return random.choice(defaults)
+    print(defaults_used)
+    answers_to_use = get_min_used_replies(defaults_used)
+    used_answer = random.choice(answers_to_use)
+    defaults_used[used_answer] += 1
+    return used_answer
 
 
 def get_answer(query: str) -> Optional[str]:
-    for statements, answers in templates:
+    for i, (statements, answers) in enumerate(templates):
         for statement in statements:
             result = re.search(statement.lower(), query.lower())
             if result:
+                possible_replies = templates_used[i][1]
+                answers_to_use = get_min_used_replies(possible_replies)
+                print(answers_to_use)
                 groups = result.groups()
+                used_answer = random.choice(answers_to_use)
+                possible_replies[used_answer] += 1
                 if groups:
-                    return random.choice(answers).format(*groups)
+                    return used_answer.format(*groups)
                 else:
-                    return random.choice(answers)
+                    return used_answer
     return None
 
 
@@ -38,5 +57,5 @@ if __name__ == '__main__':
             break
         answer = get_answer(userInput)
         if not answer:
-            answer = random.choice(defaults)
+            answer = get_default_answer()
         print(answer)
